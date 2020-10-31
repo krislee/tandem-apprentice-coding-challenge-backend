de2a4cdb736e5cbe5d3c0ed170e3f30f5bc6b8de
Kristy Lee <leekristy01@gmail.com>
2020-10-31 11:45:58 -0400

    first commit: server is set up

---
+++ server.js
@@ -0,0 +1,43 @@
+// DEPENDENCIES
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+const app = express()
+const json = require('../Apprentice_TandemFor400_Data.json')
+
+// GLOBAL VARIABLE
+const PORT = process.env.PORT
+const NODE_ENV = process.env.NODE_ENV
+
+// CORS
+const whitelist = ["http://localhost:3000/"]
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(
+          new Error("Not allowed by CORS, domain needs to be added to whitelist")
+        );
+      }
+    },
+  };
+  
+NODE_ENV === "development" ? app.use(cors()) : app.use(cors(corsOptions));
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(morgan("dev"))
+
+
+
+app.get('/trivia', function(req, res) {
+    console.log(json)
+    res.json(json)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening to ${PORT}`)
+})
